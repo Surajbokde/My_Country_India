a1ab6da75b71b157ed12b99c02c1a176b2e57b65
surajbokde <surajbokde890@gmail.com>
2021-01-30 23:05:25 +0530

    Updates

---
+++ precache-manifest.ca1e7d5a07b8c9f47e1b34d552156338.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4882f583da69804692bcfa057606ab42",
+    "url": "/My_Country_India/index.html"
+  },
+  {
+    "revision": "b0138731e530e197f1f0",
+    "url": "/My_Country_India/static/css/main.c9edb57a.chunk.css"
+  },
+  {
+    "revision": "6d9d0df2ecfe004f78d8",
+    "url": "/My_Country_India/static/js/2.f99ef8ec.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/My_Country_India/static/js/2.f99ef8ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0138731e530e197f1f0",
+    "url": "/My_Country_India/static/js/main.33da79ec.chunk.js"
+  },
+  {
+    "revision": "9f984ce73a8116e59bfa",
+    "url": "/My_Country_India/static/js/runtime-main.fafa55ee.js"
+  }
+]);
